e4aa496d41b9a237781bed47358315ee54ee256a
JackFox77 <jacobfox000@googlemail.com>
2022-08-04 16:00:37 -0600

    mark 1

--- app/Controllers/GiftsController.js
+++ app/Controllers/GiftsController.js
@@ -2,11 +2,19 @@ import { Pop } from "../Utils/Pop.js"
 import { giftsService } from '../Services/GiftsService.js'
 import { ProxyState } from "../AppState.js"
 
+function _drawGifts() {
+    let template = ''
+    ProxyState.gifts.forEach(g=> template+= g.Template)
+    document.getElementById('gifts').innerHTML = template
+}
+
 
 export class GiftsController{
     constructor(){
         console.log('Gift Controller laoded')
         this.getGifts()
+        _drawGifts()
+        ProxyState.on('gifts',_drawGifts)
     }
 
    async getGifts(){
--- app/Models/Gift.js
+++ app/Models/Gift.js
@@ -6,4 +6,14 @@ export class Gift{
         this.name = data.name,
         this.opened = data.opened
     }
+
+    get Template(){
+        return `
+    
+        <div class="col-md-4 card elevation-2 selectable no-select my-3">
+        <img  src="${this.url}" alt="">
+        </div>
+    
+        `
+    }
 }
